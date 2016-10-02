9e047ec9d07ce2460f06ee3b6dbc5c2e8505a78b
MrFibunacci <mrfibunacci@gmail.com>
2016-10-02 08:36:10 +0200

    dynamic content

---
+++ src/BuildCont.php
@@ -0,0 +1,11 @@
+<?php 
+    class BuildCont {
+        public function content(){
+            switch($_GET['p']){
+                
+            }
+            
+            return $out;
+        }
+    }
+?>
--- src/index.php
+++ src/index.php
@@ -1,8 +1,15 @@
 <?php
     include_once("Helper.php");
+    include_once("BuildCont.php");
+
     $H    = new Helper;
+    $BC   = new BuildCont;
 
     $pc   = $H->getPage("main");
+    
+    $m = array(
+        'content' => $BC->content()
+    );
 
-    echo $pc;
+    echo $H->replaceMarkers($m, $pc);
 ?>
